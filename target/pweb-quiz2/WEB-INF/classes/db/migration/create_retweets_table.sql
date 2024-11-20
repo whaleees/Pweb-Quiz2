cef1f61e7adce650d95dcf6abc03b7dee341302e
andeluw <japawallace16@gmail.com>
2024-11-20 23:40:46 +0700

    chore: add .gitignore

--- target/pweb-quiz2/WEB-INF/classes/db/migration/create_retweets_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE retweets (
-    id INT AUTO_INCREMENT PRIMARY KEY,         
-    user_id INT NOT NULL,                      
-    tweet_id INT NOT NULL,                     
-    retweeted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (tweet_id) REFERENCES tweets(id) ON DELETE CASCADE
-);
